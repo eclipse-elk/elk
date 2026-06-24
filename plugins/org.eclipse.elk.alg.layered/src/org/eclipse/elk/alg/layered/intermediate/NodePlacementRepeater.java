4eb720bbbb9f9751eb1e5f0c92a7a992c1a79b1e
Max Kasperowski <mka@informatik.uni-kiel.de>
2026-06-24 10:45:24 +0200

    make node flexibility node placement recomputation configurable

--- plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/intermediate/NodePlacementRepeater.java
+++ plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/intermediate/NodePlacementRepeater.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2026 sdo and others.
+ * Copyright (c) 2026 Kiel University and others.
  * 
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -9,32 +9,22 @@
  *******************************************************************************/
 package org.eclipse.elk.alg.layered.intermediate;
 
-import org.eclipse.elk.alg.common.nodespacing.internal.NodeContext;
-import org.eclipse.elk.alg.common.nodespacing.internal.algorithm.CellSystemConfigurator;
-import org.eclipse.elk.alg.common.nodespacing.internal.algorithm.HorizontalPortPlacementSizeCalculator;
-import org.eclipse.elk.alg.common.nodespacing.internal.algorithm.InsidePortLabelCellCreator;
-import org.eclipse.elk.alg.common.nodespacing.internal.algorithm.NodeLabelAndSizeUtilities;
-import org.eclipse.elk.alg.common.nodespacing.internal.algorithm.NodeSizeCalculator;
-import org.eclipse.elk.alg.common.nodespacing.internal.algorithm.PortContextCreator;
-import org.eclipse.elk.alg.common.nodespacing.internal.algorithm.PortLabelPlacementCalculator;
-import org.eclipse.elk.alg.common.nodespacing.internal.algorithm.PortPlacementCalculator;
-import org.eclipse.elk.alg.common.nodespacing.internal.algorithm.VerticalPortPlacementSizeCalculator;
-import org.eclipse.elk.alg.layered.DebugUtil;
+import org.eclipse.elk.alg.common.nodespacing.NodeLabelAndSizeCalculator;
+import org.eclipse.elk.alg.layered.LayeredPhases;
 import org.eclipse.elk.alg.layered.graph.LGraph;
 import org.eclipse.elk.alg.layered.graph.LGraphAdapters;
 import org.eclipse.elk.alg.layered.graph.LNode;
 import org.eclipse.elk.alg.layered.graph.LNode.NodeType;
 import org.eclipse.elk.alg.layered.graph.Layer;
-import org.eclipse.elk.alg.layered.options.FixedAlignment;
 import org.eclipse.elk.alg.layered.options.LayeredOptions;
-import org.eclipse.elk.alg.layered.p4nodes.bk.BKNodePlacer;
+import org.eclipse.elk.core.alg.ILayoutPhase;
 import org.eclipse.elk.core.alg.ILayoutProcessor;
 import org.eclipse.elk.core.util.IElkProgressMonitor;
 import org.eclipse.elk.core.util.adapters.GraphAdapters.GraphAdapter;
-import org.eclipse.elk.core.util.adapters.GraphAdapters.NodeAdapter;
 
 /**
- * @author sdo
+ * Post-processor for the node flexibility option of network simplex. Fixes the node sizes according to the output of
+ * node flexibility, then disables the option and computes node placement again with the set node placement algorithm.
  *
  */
 public class NodePlacementRepeater implements ILayoutProcessor<LGraph> {
@@ -44,22 +34,19 @@ public class NodePlacementRepeater implements ILayoutProcessor<LGraph> {
      */
     @Override
     public void process(LGraph graph, IElkProgressMonitor progressMonitor) {
-//        NetworkSimplexPlacer test = new NetworkSimplexPlacer();
-//        SimpleNodePlacer test = new SimpleNodePlacer();
-        BKNodePlacer test = new BKNodePlacer();
-        graph.setProperty(LayeredOptions.NODE_PLACEMENT_BK_FIXED_ALIGNMENT, FixedAlignment.BALANCED);
+        if (!graph.hasProperty(LayeredOptions.NODE_PLACEMENT_NETWORK_SIMPLEX_NODE_FLEXIBILITY_RECOMPUTE_NODE_PLACEMENT)) {
+            return;
+        }
+        ILayoutPhase<LayeredPhases, LGraph> nodePlacer = graph.getProperty(LayeredOptions.NODE_PLACEMENT_NETWORK_SIMPLEX_NODE_FLEXIBILITY_RECOMPUTE_NODE_PLACEMENT).create();
+        // disable node flexibility for second node placement run
         graph.setProperty(LayeredOptions.NODE_PLACEMENT_NETWORK_SIMPLEX_NODE_FLEXIBILITY, null);
         graph.setProperty(LayeredOptions.NODE_PLACEMENT_NETWORK_SIMPLEX_NODE_FLEXIBILITY_DEFAULT, null);
         for (Layer layer : graph) {
             for (LNode node : layer.getNodes()) {
-                node.setProperty(LayeredOptions.NODE_PLACEMENT_NETWORK_SIMPLEX_NODE_FLEXIBILITY, null);
-                node.setProperty(LayeredOptions.NODE_PLACEMENT_NETWORK_SIMPLEX_NODE_FLEXIBILITY_DEFAULT, null);
+                // default node size constraints == fixed size
                 node.setProperty(LayeredOptions.NODE_SIZE_CONSTRAINTS, null);
+                // reset node positions
                 node.getPosition().y = 0;
-//                node.setProperty(LayeredOptions.PORT_CONSTRAINTS, PortConstraints.FIXED_ORDER);
-                
-//                NodeContext nodeContext = new NodeContext(graph, node);
-//                PortContextCreator.createPortContexts(nodeContext, ignoreInsidePortLabels);
             }
         }
         GraphAdapter<LGraph> adapterGraph = LGraphAdapters.adapt(
@@ -67,52 +54,9 @@ public class NodePlacementRepeater implements ILayoutProcessor<LGraph> {
                 true,
                 true,
                 node -> node.getType() == NodeType.NORMAL);
-        for (NodeAdapter<?> node : adapterGraph.getNodes()) {
-            NodeContext nodeContext = new NodeContext(adapterGraph, node);
-            PortContextCreator.createPortContexts(nodeContext, true);
-            
-            InsidePortLabelCellCreator.createInsidePortLabelCells(nodeContext);
-            
-            NodeLabelAndSizeUtilities.setupMinimumClientAreaSize(nodeContext);
-            NodeLabelAndSizeUtilities.setupNodePaddingForPortsWithOffset(nodeContext);
-            
-            HorizontalPortPlacementSizeCalculator.calculateHorizontalPortPlacementSize(nodeContext);
-            VerticalPortPlacementSizeCalculator.calculateVerticalPortPlacementSize(nodeContext);
-            
-            CellSystemConfigurator.configureCellSystemSizeContributions(nodeContext);
-            
-            NodeSizeCalculator.setNodeWidth(nodeContext);
-            
-            PortPlacementCalculator.placeHorizontalPorts(nodeContext);
-            PortLabelPlacementCalculator.placeHorizontalPortLabels(nodeContext);
-            
-            CellSystemConfigurator.updateVerticalInsidePortLabelCellPadding(nodeContext);
-            
-            NodeSizeCalculator.setNodeHeight(nodeContext);
-            
-            NodeLabelAndSizeUtilities.offsetSouthernPortsByNodeSize(nodeContext);
-
-            PortPlacementCalculator.placeVerticalPorts(nodeContext);
-            PortLabelPlacementCalculator.placeVerticalPortLabels(nodeContext);
-
-            NodeLabelAndSizeUtilities.setNodePadding(nodeContext);
-            NodeLabelAndSizeUtilities.applyStuff(nodeContext);
-        }
-//        graph.setProperty(InternalProperties.WAS_FLEXIBLE, true);
-
-        // elkjs-exclude-start
-        if (progressMonitor.isLoggingEnabled()) {
-            DebugUtil.logDebugGraph(progressMonitor, graph, 0, "etst");
-        }
-        // elkjs-exclude-end
-        test.process(graph, progressMonitor);
-
-        // elkjs-exclude-start
-        if (progressMonitor.isLoggingEnabled()) {
-            DebugUtil.logDebugGraph(progressMonitor, graph, 0, "test1");
-        }
-        // elkjs-exclude-end
-
+        // recompute port positions based on now fixed node sizes
+        NodeLabelAndSizeCalculator.process(adapterGraph);
+        // second node placement run
+        nodePlacer.process(graph, progressMonitor);
     }
-
 }
